fa3c93b9a053d5e42811ce8bba6d5abebf2ed623
Jimmy Chan <jchan85@gatech.edu>
2023-01-05 23:06:03 -0800

    HOC, Smart Contract, Home Page Setup

---
+++ client/src/contract/index.js
@@ -0,0 +1,5 @@
+import contract from './AVAXGods.json';
+
+export const ADDRESS = '0xD1CD8509E95Ac34B5cE06d2a4e5aa8e8dAc900b4';
+
+export const { abi: ABI } = contract;
--- client/src/page/Home.jsx
+++ client/src/page/Home.jsx
@@ -1,13 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { PageHOC, CustomInput, CustomButton } from '../components';
+import { useGlobalContext } from '../context';
 
 const Home = () => {
+  const { contract, walletAddress } = useGlobalContext();
+  const [playerName, setPlayerName] = useState('');
+
+  const handleClick = async () => {
+    try {
+      const playerExists = await contract.isPlayer(walletAddress);
+
+      if (!playerExists) {
+        await contract.registerPlayer(playerName, playerName);
+      }
+    } catch (error) {
+      alert(error);
+    }
+  };
+
   return (
-    <div>
-      <h1 className="text-5xl p-3">Avax Gods</h1>
+    <div className="flex flex-col">
+      {/* <h1 className="text-5xl p-3">Avax Gods</h1>
       <h2 className="text-3xl p-3">Web3 NFT Battle-style Card Game</h2>
-      <p className="text-xl p-3">Made with 💜 by JavaScript Mastery</p>
+      <p className="text-xl p-3">Made with 💜 by JavaScript Mastery</p> */}
+
+      {/* <h1 className="text-white text-xl">Hello From Home</h1> */}
+
+      <CustomInput
+        label="Name"
+        placeholder="Enter your player name"
+        value={playerName}
+        handleValueChange={setPlayerName}
+      />
+
+      <CustomButton
+        title="Register"
+        handleClick={handleClick}
+        restStyles="mt-6"
+      />
     </div>
-  )
+  );
 };
 
-export default Home;
+export default PageHOC(
+  Home,
+  <>
+    Welcome to Avax Gods <br /> a Web3 NFT Card Game
+  </>,
+  <>
+    Connect your wallet to start playing <br /> the ultimate Web3 Battle Card
+    Game
+  </>
+);
